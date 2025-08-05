de69ba867aab4f9b9c7511e3aec1cea56e601341
Gabriel Rangel <gabrielrglpro@gmail.com>
2025-08-05 17:19:43 -0300

    Implementando um ItemProcessor

--- src/main/java/br/com/alura/codetickets/Importacao.java
+++ src/main/java/br/com/alura/codetickets/Importacao.java
@@ -28,6 +28,8 @@ public class Importacao {
 
     private LocalDateTime horaImportacao;
 
+    private Double taxaAdm;
+
     public Long getId() {
         return id;
     }
@@ -98,5 +100,13 @@ public class Importacao {
 
     public void setHoraImportacao(LocalDateTime horaImportacao) {
         this.horaImportacao = horaImportacao;
+    }
+
+    public Double getTaxaAdm() {
+        return taxaAdm;
+    }
+
+    public void setTaxaAdm(Double taxaAdm) {
+        this.taxaAdm = taxaAdm;
     }
 }
--- src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
+++ src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
@@ -40,6 +41,7 @@ public class ImportacaoJobConfiguration {
         return new StepBuilder("passo-inicial", jobRepository)
                 .<Importacao, Importacao>chunk(200, transactionManager)
                 .reader(reader)
+                .processor(processor())
                 .writer(writer)
                 .build();
     }
@@ -62,10 +64,15 @@ public class ImportacaoJobConfiguration {
         return new JdbcBatchItemWriterBuilder<Importacao>()
                 .dataSource(dataSource)
                 .sql(
-                    "INSERT INTO importacao (cpf, cliente, evento, data, tipo_ingresso, valor, hora_importacao) VALUES " +
-                            "(:cpf, :cliente, :evento, :data, :tipoIngresso, :valor, :horaImportacao)"
+                    "INSERT INTO importacao (cpf, cliente, nascimento, evento, data, tipo_ingresso, valor, hora_importacao, taxa_adm) VALUES " +
+                            "(:cpf, :cliente, :nascimento, :evento, :data, :tipoIngresso, :valor, :horaImportacao, :taxaAdm)"
                 )
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .build();
+    }
+
+    @Bean
+    public ImportacaoProcessor processor() {
+        return new ImportacaoProcessor();
     }
 }
